54c21146c5787bc5ba979828d645782728aac355
GhostEd123 <djemerhomumirarosue43@gmail.com>
2022-02-02 17:10:12 -0800

    another commit

---
+++ assignment1.dart
@@ -0,0 +1,10 @@
+import "dart:io";
+void main(){
+    print("What is your name?");
+    String? name = stdin.readLineSync();
+    print('What department did you graduate from?');
+    String? department = stdin.readLineSync();
+    print('What was your CGPA?');
+    String? CGPA = stdin.readLineSync();
+    print('Hello $name, you graduated with $CGPA from the department of $department');
+}
